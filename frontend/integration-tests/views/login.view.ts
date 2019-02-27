3d4878a40444dcd4a01f47b3f2fc04f5ff2b6857
Guohua Ouyang <gouyang@redhat.com>
2019-02-27 15:57:52 +0800

    fix the login scenario
    Signed-off-by: Guohua Ouyang <gouyang@redhat.com>

--- frontend/integration-tests/views/login.view.ts
+++ frontend/integration-tests/views/login.view.ts
@@ -2,6 +2,6 @@
 
 import { $ } from 'protractor';
 
-export const nameInput = $('input[name=login]');
+export const nameInput = $('input[name=username]');
 export const passwordInput = $('input[name=password]');
 export const submitButton = $('button[type=submit]');
