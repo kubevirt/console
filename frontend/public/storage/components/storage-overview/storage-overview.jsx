5e919c47f3fdd8d1cc2cd5a667209c5a2eaf7a15
Neha <gnehapk@gmail.com>
2019-03-27 13:32:36 +0530

    Integrated inventory card

--- frontend/public/storage/components/storage-overview/storage-overview.jsx
+++ frontend/public/storage/components/storage-overview/storage-overview.jsx
@@ -1,8 +1,68 @@
 import React from 'react';
 import {
   StorageOverview as KubevirtStorageOverview,
-
+  ClusterOverviewContext,
+  getResource,
 } from 'kubevirt-web-ui-components';
+
+import { NodeModel, PodModel, PersistentVolumeClaimModel, VirtualMachineModel, InfrastructureModel } from '../../../models';
+import { WithResources } from '../../../kubevirt/components/utils/withResources';
+
+const resourceMap = {
+  nodes: {
+    resource: getResource(NodeModel, {namespaced: false}),
+  },
+  pods: {
+    resource: getResource(PodModel),
+  },
+  pvcs: {
+    resource: getResource(PersistentVolumeClaimModel),
+  },
+  vms: {
+    resource: getResource(VirtualMachineModel),
+  },
+  infrastructure: {
+    resource: getResource(InfrastructureModel, { namespaced: false, name: 'cluster', isList: false }),
+  },
+};
+
+const getInventoryData = resources => {
+  const inventory = {};
+  if (resources.nodes) {
+    inventory.nodes = {
+      data: resources.nodes,
+      title: 'Hosts',
+      kind: NodeModel.kind,
+    };
+  }
+  if (resources.pods) {
+    inventory.pods = {
+      data: resources.pods,
+      title: 'Pods',
+      kind: PodModel.kind,
+    };
+  }
+  if (resources.pvcs) {
+    inventory.pvcs = {
+      data: resources.pvcs,
+      title: 'PVCs',
+      kind: PersistentVolumeClaimModel.kind,
+    };
+  }
+  if (resources.vms) {
+    inventory.vms = {
+      data: resources.vms,
+      title: 'VMs',
+      kind: VirtualMachineModel.kind,
+    };
+  }
+
+  return {
+    inventory,
+    loaded: !!inventory,
+    heading: 'OCS Inventory',
+  };
+};
 
 export class StorageOverview extends React.Component {
   constructor(props){
@@ -11,8 +71,20 @@ export class StorageOverview extends React.Component {
   }
 
   render() {
+    const inventoryResourceMapToProps = resources => {
+      return {
+        value: {
+          inventoryData: getInventoryData(resources), // k8s object loaded via WithResources
+        },
+      };
+    };
+
     return (
-      <KubevirtStorageOverview />
+      <WithResources resourceMap={resourceMap} resourceToProps={inventoryResourceMapToProps}>
+        <ClusterOverviewContext.Provider>
+          <KubevirtStorageOverview />
+        </ClusterOverviewContext.Provider>
+      </WithResources>
     );
   }
 }
