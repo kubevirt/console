d8f69daa2c0bc411fdff7f939102852b71431b8c
Jadhaj <jhajyahy@redhat.com>
2019-06-03 18:04:50 +0300

    fixed sidenav.view.ts navigation issue fixed lint checks removed empty lines

--- frontend/integration-tests/views/sidenav.view.ts
+++ frontend/integration-tests/views/sidenav.view.ts
@@ -2,11 +2,15 @@
 
 import { $$, by, browser, element, ExpectedConditions as until } from 'protractor';
 
-export const navSectionFor = (name: string) => element(by.cssContainingText('.pf-c-nav > .pf-c-nav__list > .pf-c-nav__item', name));
+export const navSectionFor = (name: string) => element(by.linkText(name));
 
 export const clickNavLink = (path: [string, string]) => browser.wait(until.visibilityOf(navSectionFor(path[0])))
-  .then(() => navSectionFor(path[0]).click())
-  .then(() => browser.wait(until.visibilityOf(navSectionFor(path[0]).element(by.linkText(path[1])))))
-  .then(() => navSectionFor(path[0]).element(by.linkText(path[1])).click());
+  .then(() => navSectionFor(path[0]).getAttribute('aria-expanded').then(function(value) {
+    if (value === 'false') {
+      navSectionFor(path[0]).click();
+    }
+  }))
+  .then(() => browser.wait(until.visibilityOf(navSectionFor(path[1]))))
+  .then(() => navSectionFor(path[1]).click());
 
 export const activeLink = $$('.pf-c-nav__link.pf-m-current');
